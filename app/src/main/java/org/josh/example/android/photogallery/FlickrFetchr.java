f6690e9be28476b572fc8efae399210a8552c0fc
Josh-Skrzypczak <jskrzypczak@house.mi.gov>
2015-09-25 22:40:23 -0400

    Parsing and ViewHolder

--- app/src/main/java/org/josh/example/android/photogallery/FlickrFetchr.java
+++ app/src/main/java/org/josh/example/android/photogallery/FlickrFetchr.java
@@ -1,10 +1,18 @@
 package org.josh.example.android.photogallery;
 
+import android.net.Uri;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Josh on 9/24/2015.
@@ -14,10 +22,10 @@ import java.net.URL;
     //once url is created and open connection, call read repeatedly until your connection runs out of data. The inputstream
     //yields bytes as they're available.
 
-
-
-
 public class FlickrFetchr {
+
+    private static final String TAG = "FlickrFetchr";
+    private static final String API_KEY ="b5a4d6877915b52957ea8d40ce58eb92";
 
     public byte[] getUrlBytes(String urlSepc) throws IOException{
         URL url = new URL(urlSepc);
@@ -44,6 +52,51 @@ public class FlickrFetchr {
     }
     public String getUrlString(String urlSpec)throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    public List<GalleryItem> fetchItems(){
+
+        List<GalleryItem> items = new ArrayList<>();
+
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/")
+                    .buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("extras", "url_s")
+                    .build().toString();
+            String jsonString = getUrlString(url);
+            //Log.i(TAG, "Received JSON: " + jsonString);
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItems(items, jsonBody);
+        }catch (JSONException je){
+            //Log.e(TAG, "Failed to parse JSON", je);
+        }catch (IOException ioe){
+            //Log.e(TAG, "Failed to fetch items", ioe);
+        }
+        return items;
+    }
+
+    private void parseItems(List<GalleryItem> items, JSONObject jsonBody) throws IOException, JSONException{
+
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+
+        for (int i = 0; i < photoJsonArray.length(); i++){
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+
+            if (!photoJsonObject.has("url_s")){
+                continue;
+            }
+            item.setUrl(photoJsonObject.getString("url_s"));
+            items.add(item);
+        }
     }
 
 }
--- app/src/main/java/org/josh/example/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/org/josh/example/android/photogallery/PhotoGalleryFragment.java
@@ -6,31 +6,42 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.io.IOException;
+import android.widget.TextView;
 
 /**
  * Created by Josh on 9/24/2015.
  */
+
+//FLICKR KEY
+    //b5a4d6877915b52957ea8d40ce58eb92
+    //https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=xxx&format=json&nojsoncallback=1
 public class PhotoGalleryFragment extends Fragment {
 
     private static final String TAG = "PhotoGalleryFragment";
 
     private RecyclerView mPhotoRecyclerView;
 
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+
+        public PhotoHolder(View itemView){
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+        public void bindGalleryItem(GalleryItem item){
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+
     private class FetchItemsTask extends AsyncTask<Void, Void, Void>{
         @Override
         protected Void doInBackground(Void... params) {
-            try {
-                String result = new FlickrFetchr().getUrlString("https://www.bignerdranch.com");
-                Log.i(TAG, "Fetched contents of URL: " + result);
-            }catch (IOException ioe){
-                Log.e(TAG, "Failed to fetch URL: ", ioe);
-            }
+           new FlickrFetchr().fetchItems();
             return null;
         }
     }
@@ -43,6 +54,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
